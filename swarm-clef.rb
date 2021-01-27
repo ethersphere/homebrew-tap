52f47938e2d68fd892a6c3a7170708521e300518
bee-worker <bee-worker@ethswarm.org>
2021-01-27 16:24:13 +0100

    Brew formula update for bee-clef version v0.4.6

--- swarm-clef.rb
+++ swarm-clef.rb
@@ -2,12 +2,12 @@
 class SwarmClef < Formula
   desc "Ethereum Clef"
   homepage "https://swarm.ethereum.org/"
-  version "0.4.5"
+  version "0.4.6"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/ethersphere/bee-clef/releases/download/v0.4.5/bee-clef-darwin-amd64.tar.gz"
-    sha256 "649f768c93a63b61a38bb94adf29a3b578dac5a0f8436f7e9b0382901c8176f2"
+    url "https://github.com/ethersphere/bee-clef/releases/download/v0.4.6/bee-clef-darwin-amd64.tar.gz"
+    sha256 "1e3f2b3a015960d558334c75d174ff69b95222c58a49d13282f44af2dacb3661"
   end
 
   def install
