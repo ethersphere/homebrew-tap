a457005176d96976022b9106da52831c86f300ac
bee-worker <bee-worker@ethswarm.org>
2021-05-22 11:52:22 +0200

    Brew formula update for bee version v0.6.1

--- swarm-bee.rb
+++ swarm-bee.rb
@@ -5,12 +5,12 @@
 class SwarmBee < Formula
   desc "Ethereum Swarm node"
   homepage "https://swarm.ethereum.org/"
-  version "0.6.0"
+  version "0.6.1"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/ethersphere/bee/releases/download/v0.6.0/bee-darwin-amd64.tar.gz"
-    sha256 "37f4e00c84b07dfffa73d2023cb11192b604959da48d1ecac83753564098ee1c"
+    url "https://github.com/ethersphere/bee/releases/download/v0.6.1/bee-darwin-amd64.tar.gz"
+    sha256 "969e6c0f82c2e10935b1c4e19f8d3fb00ff899e808947df6d2904209fe3e427a"
   end
 
   def install
