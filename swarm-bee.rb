393df82482963b8e34e83d833a0f8848fbe00f40
bee-worker <bee-worker@ethswarm.org>
2021-10-18 20:22:20 +0200

    Brew formula update for bee version v1.3.0-rc1

--- swarm-bee.rb
+++ swarm-bee.rb
@@ -5,14 +5,14 @@
 class SwarmBee < Formula
   desc "Ethereum Swarm node"
   homepage "https://swarm.ethereum.org/"
-  version "1.2.0"
+  version "1.3.0-rc1"
   bottle :unneeded
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/ethersphere/bee/releases/download/v1.2.0/bee-darwin-amd64.tar.gz"
-      sha256 "425732f8fb86dc3da8a38dc8caff7028686b2aaf0067a46501a47982912a51a9"
+      url "https://github.com/ethersphere/bee/releases/download/v1.3.0-rc1/bee-darwin-amd64.tar.gz"
+      sha256 "2e465103ae727ab84ef1a13d585b0299011fd9f7dd7d8f226326eadb9e1200aa"
     end
   end
 
