3bf49b3bc43bc34ff9ef6482c0e22591dfadc8ca
bee-worker <bee-worker@ethswarm.org>
2023-06-20 14:19:50 +0200

    Brew formula update for bee version v2.0.0-rc2

--- swarm-bee.rb
+++ swarm-bee.rb
@@ -5,13 +5,13 @@
 class SwarmBee < Formula
   desc "Ethereum Swarm node"
   homepage "https://swarm.ethereum.org/"
-  version "2.0.0-rc1"
+  version "2.0.0-rc2"
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/ethersphere/bee/releases/download/v2.0.0-rc1/bee-darwin-amd64.tar.gz"
-      sha256 "e8359b6341b0bdb3060c259bca364465b2bb433987740fc32951d15136651cf9"
+      url "https://github.com/ethersphere/bee/releases/download/v2.0.0-rc2/bee-darwin-amd64.tar.gz"
+      sha256 "6d5c277765f25b6e544451b4d8dee7ff08acdbc8f622780d6759e52d10f875f9"
 
       def install
         (etc/"swarm-bee").mkpath
@@ -21,8 +21,8 @@ class SwarmBee < Formula
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/ethersphere/bee/releases/download/v2.0.0-rc1/bee-darwin-arm64.tar.gz"
-      sha256 "dd690f2beeb17c9c9056e7179a66fe057b6a0edb3456f6417b81612e40ae8782"
+      url "https://github.com/ethersphere/bee/releases/download/v2.0.0-rc2/bee-darwin-arm64.tar.gz"
+      sha256 "6e0919ac3d993975dde10bab4e1d3884252bfb27421ac99d3e26dcc6054e59dd"
 
       def install
         (etc/"swarm-bee").mkpath
